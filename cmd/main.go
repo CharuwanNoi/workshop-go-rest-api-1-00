3d6c587010fe09d99735cf306cc22611bbe12994
Charuwan Ruanmoei <charuwan2537@gmail.com>
2021-08-07 11:40:14 +0700

    refactor step 1

---
+++ cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"database/sql"
+	"demo"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+// var DB *sql.DB
+type Resource struct {
+	db *sql.DB
+}
+
+func main() {
+	// DB = createDatabaseConnection()
+	r := createDatabaseConnection()
+	// users, _ := getAllUsers(db)
+	// fmt.Printf("%+v", users)
+
+	router := gin.New()
+	route := router.Group("/api/v1")
+	route.GET("/user", handleGetUsers(r))
+	router.Run(":8080")
+
+}
+
+// Closure
+func handleGetUsers(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		users, _ := demo.GetAllUsers(db)
+		c.JSON(http.StatusOK, users)
+	}
+}
+
+func createDatabaseConnection() *sql.DB {
+	var db *sql.DB
+	var err error
+
+	db, err = sql.Open("postgres", "postgres://user:pass@localhost/demo?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping DB: ", err)
+	}
+	return db
+}
